d30c36d1419633532771514e25c12cb4b8e759e5
William Hibberd <will@williamhibberd.co.uk>
2024-12-06 01:00:41 +0000

    enhance media gallery functionality; update layout and item tracking

--- src/scripts/components/mediaGallery.js
+++ src/scripts/components/mediaGallery.js
@@ -1,26 +1,43 @@
 import Lenis from "lenis";
 
 export default function mediaGallery() {
-	return {
-		init() {
-			const tail = this.$refs.tail;
-			const root = this.$root;
+	const items = this.$root.querySelectorAll("[data-item]");
 
-			// Initialize Lenis
-			const lenis = new Lenis({
-				autoRaf: true,
-				wrapper: root,
-				orientation: "horizontal",
-			});
+	return {
+		currentItem: "01",
+		totalItems: items.length.toString().padStart(2, "0"),
+
+		init() {
+			const container = this.$refs.container;
+			const tail = this.$refs.tail;
+
+			// // Initialize Lenis
+			// const lenis = new Lenis({
+			// 	autoRaf: true,
+			// 	wrapper: container,
+			// 	orientation: "horizontal",
+			// });
+
+			// container.addEventListener("scroll", () => {
+			// 	items.forEach((item, i) => {
+			// 		const rect = item.getBoundingClientRect();
+			// 		const isInViewAndFurthestLeft =
+
+			// 		if (isInViewAndFurthestLeft) {
+			// 			this.currentItem = (i + 1).toString().padStart(2, "0");
+			// 			console.log(this.currentItem);
+			// 		}
+			// 	});
+			// });
 
 			window.addEventListener("mousemove", (e) => {
 				tail.style.top = `${e.clientY - 40}px`;
 				tail.style.left = `${e.clientX - 40}px`;
 			});
-			root.addEventListener("mouseover", () => {
+			container.addEventListener("mouseover", () => {
 				tail.classList.remove("opacity-0");
 			});
-			root.addEventListener("mouseout", () => {
+			container.addEventListener("mouseout", () => {
 				tail.classList.add("opacity-0");
 			});
 		},
